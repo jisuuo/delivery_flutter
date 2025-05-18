47eb147a3f7336295a77aa455faca4e0bb27336c
jissuo <jisu2298@gmail.com>
2025-05-18 18:04:31 +0900

    20250518 1. TabBarView 구현

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -3,13 +3,32 @@ import 'package:flutter/material.dart';
 class DefaultLayout extends StatelessWidget {
   final Color? backgroundColor;
   final Widget child;
-  const DefaultLayout({super.key, required this.child, this.backgroundColor});
+  final String? title;
+  final Widget? bottomNavigationBar;
+
+  const DefaultLayout({super.key, required this.child, this.backgroundColor, this.title, this.bottomNavigationBar});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppBar(),
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
     );
   }
+
+  AppBar? renderAppBar() {
+    if(title == null) {
+      return null;
+    } else {
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(title!, style: TextStyle(fontSize: 16.0),),
+        foregroundColor: Colors.black,
+      );
+    }
+  }
 }
+
