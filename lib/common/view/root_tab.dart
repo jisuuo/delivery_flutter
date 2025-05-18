e0588dbd7cbf9c855673505c06309c1a8b5ee0ce
jissuo <jisu2298@gmail.com>
2025-05-18 18:32:18 +0900

    20250518 1. restaurant_screen / restaraunt_card 공통 위젯 구현

--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -1,5 +1,6 @@
 import 'package:delivery_flutter/common/const/colors.dart';
 import 'package:delivery_flutter/common/layout/default_layout.dart';
+import 'package:delivery_flutter/restaurant/view/restaurant_screen.dart';
 import 'package:flutter/material.dart';
 
 class RootTab extends StatefulWidget {
@@ -67,7 +68,7 @@ class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin{
         physics: NeverScrollableScrollPhysics(),
         controller: controller,
         children: [
-          Center(child: Container(child: Text('홈'))),
+          RestaurantScreen(),
           Center(child: Container(child: Text('음식'))),
           Center(child: Container(child: Text('주문'))),
           Center(child: Container(child: Text('프로필'))),
