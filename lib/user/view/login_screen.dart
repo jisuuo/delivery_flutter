8d17452c9eb1f0d23fcf71e03daf6928d9019916
jissuo <jisu2298@gmail.com>
2025-05-18 10:41:44 +0900

    20250518 1. dio 패키지 사용 / 로그인, 리프레시 토큰 갱신 구현

--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -1,13 +1,36 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:delivery_flutter/common/component/custom_text_form_field.dart';
 import 'package:delivery_flutter/common/const/colors.dart';
 import 'package:delivery_flutter/common/layout/default_layout.dart';
+import 'package:delivery_flutter/common/view/root_tab.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String username = '';
+  String password = '';
+
+  @override
   Widget build(BuildContext context) {
+    final dio = Dio();
+
+    /// mac에서 시뮬레이터 사용할 경우는 네트워크 주소 똑같음
+    /// 이 외에는 네트워크가 다르므로 에뮬레이터 기준으로 따로 설정을 해줘야함
+    /// localhost
+    final emulatorIp = '10.0.2.2:3000';
+    final simulatorIp = '127.0.0.1:3000';
+
+    final ip = Platform.isIOS ? simulatorIp : emulatorIp;
+
     return DefaultLayout(
       child: SafeArea(
         top: true,
@@ -29,21 +52,66 @@ class LoginScreen extends StatelessWidget {
                 const SizedBox(height: 10.0),
                 CustomTextFormField(
                   hintText: '이메일을 입력해주세요.',
-                  onChanged: (String value) {},
+                  onChanged: (String value) {
+                    username = value;
+                  },
                 ),
                 const SizedBox(height: 16.0),
                 CustomTextFormField(
                   hintText: '비밀번호를 입력해주세요.',
-                  onChanged: (String value) {},
+                  onChanged: (String value) {
+                    password = value;
+                  },
                   obscureText: true,
                 ),
                 const SizedBox(height: 16.0),
+
+                /// 로그인 버튼
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
                     backgroundColor: PRIMARY_COLOR,
                   ),
-                  onPressed: () {}, child: Text('로그인', style: TextStyle(color: Colors.white),)),
-                TextButton(onPressed: () {}, child: Text('회원가입', style: TextStyle(color: Colors.black),)),
+                  onPressed: () async {
+                    /// ID:비밀번호
+                    // final rawString = '$username:$password';
+                    final rawString = 'test@codefactory.ai:testtest';
+
+                    /// base64 변환하는 메서드
+                    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+
+                    String token = stringToBase64.encode(rawString);
+
+                    final resp = await dio.post(
+                      'http://$ip/auth/login',
+                      options: Options(
+                        headers: {'authorization': 'Basic $token'},
+                      ),
+                    );
+
+                    Navigator.of(
+                      context,
+                    ).push(MaterialPageRoute(builder: (_) => RootTab()));
+                    print(resp.data);
+                  },
+                  child: Text('로그인', style: TextStyle(color: Colors.white)),
+                ),
+
+                /// 회원가입 버튼
+                TextButton(
+                  onPressed: () async {
+                    final refreshToken =
+                        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAY29kZWZhY3RvcnkuYWkiLCJzdWIiOiJmNTViMzJkMi00ZDY4LTRjMWUtYTNjYS1kYTlkN2QwZDkyZTUiLCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTc0NzUzMTYwMCwiZXhwIjoxNzQ3NjE4MDAwfQ.AkjgOfJPXDxIiu58YolgT9RB6rBZs_OrQuVZh848E6I';
+
+                    final resp = await dio.post(
+                      'http://$ip/auth/token',
+                      options: Options(
+                        headers: {'authorization': 'Bearer $refreshToken'},
+                      ),
+                    );
+                    print(resp.data);
+                  },
+                  child: Text('회원가입', style: TextStyle(color: Colors.black)),
+                ),
               ],
             ),
           ),
